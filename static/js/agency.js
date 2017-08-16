651b5c284140f2f7f08015a1e937d5ac74ded21a
Steph <Steph@itsalocke.com>
2017-08-16 12:57:44 +0100

    :mailbox: tweaks

--- static/js/agency.js
+++ static/js/agency.js
@@ -48,24 +48,22 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-// Contact form validation
-$( document ).ready(function() {
-  $.validate({
-    modules : 'html5, toggleDisabled'
-  });
-});
-
 // Async contact form
 $('form[id=contactForm]').submit(function(){
-  $.post($(this).attr('action'), $(this).serialize(), function(res){
+  $.post($(this).attr('action'), $(this).serialize(), function(data, textStatus, jqXHR){
     $('form[id=contactForm] #success').hide();
     $('form[id=contactForm] #error').hide();
-    if (res.code == "200")
+    if (jqXHR.status == 200) {
       $('form[id=contactForm] #success').show();
-    }).fail(function(){
-    $('form[id=contactForm] #success').hide();
-    $('form[id=contactForm] #error').hide();
-    $('form[id=contactForm] #error').show();
+    }}, 'json').fail(function(){
+      $('form[id=contactForm] #success').hide();
+      $('form[id=contactForm] #error').hide();
+      $('form[id=contactForm] #error').show();
   });
   return false;
+});
+
+// Contact form validation
+$.validate({
+  modules : 'html5, toggleDisabled'
 });
